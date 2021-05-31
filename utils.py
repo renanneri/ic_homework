b26ff8ee1de4b1c220549c0e3b2f234fcf7fa763
Renan Neri <renan.pereira@vtex.com.br>
2021-05-30 22:43:21 -0300

    Add things to wine analysis

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV,StratifiedKFold
 import numpy as np
+import pandas as pd
 
 
 def plot_result_score_model(results,scoring,param):
@@ -16,32 +17,36 @@ def plot_result_score_model(results,scoring,param):
 
         ax = plt.gca()
 
+        results_df = pd.DataFrame(results).groupby(['param_C']).mean()
+        max = pd.DataFrame(results).groupby(['param_C']).max(numeric_only=True)
+        min = pd.DataFrame(results).groupby(['param_C']).min(numeric_only=True)
         # Get the regular numpy array from the MaskedArray
-        X_axis = np.array(results[f'param_{param}'].data, dtype=float)
+        X_axis = results_df.index
 
-    
         for sample, style in (('train', '--'), ('test', '-')):
-            sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
-            sample_score_std = results['std_%s_%s' % (sample, scorer)]
-            ax.fill_between(X_axis, sample_score_mean - sample_score_std,
-                            sample_score_mean + sample_score_std,
+            sample_score_mean = results_df['mean_%s_%s' % (sample, scorer)]
+            min_sample = min['mean_%s_%s' % (sample, scorer)]
+            max_sample = max['mean_%s_%s' % (sample, scorer)]
+            ax.fill_between(X_axis, min_sample,max_sample,
                             alpha=0.1 if sample == 'test' else 0, color=color)
             ax.plot(X_axis, sample_score_mean, style, color=color,
                     alpha=1 if sample == 'test' else 0.7,
                     label="%s (%s)" % (scorer, sample))
 
-        best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
-        best_score = results['mean_test_%s' % scorer][best_index]
+        best_index = results_df['rank_test_%s' % scorer].sort_values(ascending=False).index[0]
+        worst_index = results_df['rank_test_%s' % scorer].sort_values(ascending=False).index[-1]
+        best_score = results_df['mean_test_%s' % scorer][best_index]
+        worst_score = results_df['mean_test_%s' % scorer][worst_index]
 
         # Plot a dotted vertical line at the best score for that scorer marked by x
         
-        ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+        ax.plot([best_index, ] * 2, [worst_score/1.1 , best_score],
                 linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
 
 
         # Annotate the best score for that scorer
         ax.annotate("%0.2f" % best_score,
-                    (X_axis[best_index] + 0.01, best_score + 0.010))
+                    (best_index + 0.01, best_score + 0.010))
 
         plt.legend(loc="best")
         plt.grid(False)
