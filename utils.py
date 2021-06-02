09d886038ed22971fe676fe49988e07ece4db27a
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-06-02 12:31:39 -0300

    Fix graph

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ def plot_result_score_model(results,scoring,param):
         ax = plt.gca()
 
         results_df = pd.DataFrame(results).groupby(['param_{}'.format(param)]).mean()
+        df = pd.DataFrame(results)
                 
         max = pd.DataFrame(results).groupby(['param_{}'.format(param)]).max(numeric_only=True)
         min = pd.DataFrame(results).groupby(['param_{}'.format(param)]).min(numeric_only=True)
@@ -34,23 +35,22 @@ def plot_result_score_model(results,scoring,param):
                     alpha=1 if sample == 'test' else 0.7,
                     label="%s (%s)" % (scorer, sample))
 
-        best_index = results_df['rank_test_%s' % scorer].sort_values(ascending=False).index[0]
-        worst_index = results_df['rank_test_%s' % scorer].sort_values(ascending=False).index[-1]
-        best_score = results_df['mean_test_%s' % scorer][best_index]
-        worst_score = results_df['mean_test_%s' % scorer][worst_index]
+#         best_index = df[ ['param_{}'.format(param), 'rank_test_%s' % scorer] ].sort_values(by='param_{}'.format(param), ascending=False).loc[0]["param_{}".format(param)]
+        
+#         worst_index = df['rank_test_%s' % scorer].sort_values(ascending=False).index[-1]
+#         best_score = df['mean_test_%s' % scorer][best_index]
+#         worst_score = df['mean_test_%s' % scorer][worst_index]
 
         # Plot a dotted vertical line at the best score for that scorer marked by x
-        
-        ax.plot([best_index, ] * 2, [worst_score/1.1 , best_score],
-                linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+#         ax.plot([best_index, ] * 2, [worst_score/1.1 , best_score],
+#                 linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
 
-
-        # Annotate the best score for that scorer
-        if(isinstance(best_index, float)):
-            ax.annotate("%0.2f" % best_score,
-                    (best_index + 0.01, best_score + 0.010))
-        else:
-            ax.annotate("%0.2f" % best_score, (best_index, best_score + 0.010))
+#         # Annotate the best score for that scorer
+#         if(isinstance(best_index, float)):
+#             ax.annotate("%0.2f" % best_score,
+#                     (best_index + 0.01, best_score + 0.010))
+#         else:
+#             ax.annotate("%0.2f" % best_score, (best_index, best_score + 0.010))
 
         plt.legend(loc="best")
         plt.grid(False)
